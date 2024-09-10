8c5155ddba6e6b3598d51b999c8ec2f0800bf919
battistaennio <ennio.battista99@gmail.com>
2024-09-10 15:24:41 +0200

    feat: add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,55 @@
+<header>
+    <div class="container">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC logo">
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="nav">
+                <li>
+                    <a href="#" class="">characters</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">comics</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">movies</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">tv</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">games</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">collectibles</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">videos</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">fans</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">news</a>
+                </li>
+
+                <li>
+                    <a href="#" class="">shop</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
