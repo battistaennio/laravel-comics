a033e21cb541406340a4b90b34d6d5eb9f3c51e2
battistaennio <ennio.battista99@gmail.com>
2024-09-10 15:34:08 +0200

    feat: add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,255 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
-</footer>
+<section id="footer-buy">
+
+    <a href="#">
+        <div class="buy-box">
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="digital comics">
+            </div>
+            <div class="shop-text">
+                <span>digital comics</span>
+            </div>
+        </div>
+    </a>
+
+    <a href="#">
+        <div class="buy-box">
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="merchandise">
+            </div>
+            <div class="shop-text">
+                <span>dc merchandise</span>
+            </div>
+        </div>
+    </a>
+
+    <a href="#">
+        <div class="buy-box">
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="subscription">
+            </div>
+            <div class="shop-text">
+                <span>subscription</span>
+            </div>
+        </div>
+    </a>
+
+    <a href="#">
+        <div class="buy-box">
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="shop locator">
+            </div>
+            <div class="shop-text">
+                <span>dc shop locator</span>
+            </div>
+        </div>
+    </a>
+
+    <a href="#">
+        <div class="buy-box">
+            <div class="img-container">
+                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="dc power visa">
+            </div>
+            <div class="shop-text">
+                <span>dc power visa</span>
+            </div>
+        </div>
+    </a>
+
+</section>
+
+<section id="footer-list_container">
+    <div class="container">
+        <div id="footer-list">
+            <div>
+                <ul>
+                    <li>
+                        <a href="#">
+                            dc comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Characters
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Movies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            TV
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Games
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Videos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            News
+                        </a>
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <a href="#">
+                            shop
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop DC
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop DC Collectibles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">
+                        dc
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Characters
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Comics
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Movies
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Games
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Videos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        News
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Games
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Videos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        News
+                    </a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <a href="#">
+                        sites
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        DC
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        MAD Magazine
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        DC Kids
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        DC Universe
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        DC Power Visa
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div id="footer-logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="footer logo">
+        </div>
+
+    </div>
+</section>
+
+<section id="footer-social">
+    <div class="container">
+        <button>sign-up now!</button>
+
+        <div id="social-container">
+            <h3>follow us</h3>
+            <div class="img-container">
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
